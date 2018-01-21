e40ef7ab16b851defde96a6f32c1c190899228cd
Onur Destanoğlu <odestanoglu@gmail.com>
2018-01-21 16:45:22 +0300

    health and readiness probes are included

---
+++ LoadGenerationService/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoadGeneratorService.Controllers
+{
+    [Route("probe")]
+    public class HealthController : Controller
+    {
+        // GET api/load/5
+        [HttpGet("health")]
+        public bool Health()
+        {
+            return true;
+        }
+
+        // GET api/load/5
+        [HttpGet("readiness")]
+        public bool Readiness()
+        {
+            return true;
+        }
+    }
+}
