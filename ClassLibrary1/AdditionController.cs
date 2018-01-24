62073fd60b9fd24d7213c1bd213eaec4d87d649b
Onur Destanoğlu <odestanoglu@gmail.com>
2018-01-24 07:51:45 +0300

    # library based controller is added for testing

---
+++ ClassLibrary1/AdditionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExtension
+{
+    [Route("api/addition")]
+    public class AdditionController : Controller
+    {
+        [HttpGet("add/{first}/{second}")]
+        public string Index(int first, int second)
+        {
+            return (first +second).ToString();
+        }
+    }
+}
